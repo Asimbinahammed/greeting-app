6cd59a9ec34c2279f6cde784a12c69dc356c0145
asimbinahammed <asim242370@gmail.com>
2021-12-03 21:08:14 +0530

    Added: using service layer to get greeting message

--- src/main/java/com/bridgelabz/greetingapp/controller/GreetingAppController.java
+++ src/main/java/com/bridgelabz/greetingapp/controller/GreetingAppController.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.greetingapp.controller;
 
+import com.bridgelabz.greetingapp.services.GreetingAppService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingAppController {
+
+    @Autowired
+    private GreetingAppService greetingAppService;
 
     @GetMapping(value = "/greeting")
     public String greetingMessage(){
@@ -21,5 +26,10 @@ public class GreetingAppController {
     @GetMapping(value = "/greeting2")
     public String greetingMessageWithPassingParameters(@RequestParam(value = "name") String name){
         return "Welcome " + name;
+    }
+
+    @GetMapping(value = "/greeting3")
+    public String greetingMessageFromServiceClass(){
+        return greetingAppService.getMessage();
     }
 }
